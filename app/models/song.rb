07094a3b1f868d12dce6b126d7e0bf74ff5a8706
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-15 08:24:51 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes, :allow_destroy => true
-  
+
   accepts_nested_attributes_for :notes
 
   def artist_name=(name)
