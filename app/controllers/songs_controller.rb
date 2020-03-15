02c19dbb51182795eb08ea4806884cd6c4e727a4
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-15 08:52:49 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -49,12 +49,10 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    
     params.require(:song).permit(
       :title,
       :artist_name,
       :genre_id
     )
-
   end
 end
