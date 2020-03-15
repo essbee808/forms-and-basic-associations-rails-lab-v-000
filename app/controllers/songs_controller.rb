c6c1ef589e59bdd2d24bf3eda2c2b2dfe03a6f84
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-15 08:11:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -53,7 +53,7 @@ class SongsController < ApplicationController
       :title,
       :artist_name,
       :genre_id,
-      notes_attributes: [:content]
+      notes_attributes: [:content_1, :content_2]
     )
   end
 end
