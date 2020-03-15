fdd3856f5382a0fde4854266e1e411878a0b5555
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-15 06:13:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -23,7 +23,7 @@ class Song < ActiveRecord::Base
 
   def notes_attributes=(array)
     array.each do |note|
-      new_note = Note.create()
+      new_note = Note.create(content: )
     end
   end
 
